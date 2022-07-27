2d8f1dc47e03de7be5920615ebf20b5f725fa0c6
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-07-28 00:33:38 +0500

    python variables

---
+++ variable.py
@@ -0,0 +1,9 @@
+name = 'Muhammad Nabeel'
+age1 = 26
+age2 = 26
+age3 = 27
+
+# id() function is use to find a variable memory location
+print(id(age1), id(age2), id(age3))
+
+print('Name : ', name,'\nAge : ', age1)
