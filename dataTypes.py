5799439ac8a8d7b1659720412e90043f81e1adbb
Nabeel-Chhipa <nabeelchipa@gmail.com>
2022-08-07 02:08:52 +0500

    Datatypes

---
+++ dataTypes.py
@@ -0,0 +1,17 @@
+# a = 10
+# print(a, 'Type => ', type(a))
+
+# b = 8.9
+# print(a, 'Type => ', type(b))
+
+# c = 1.9j
+# print(c, 'Type => ', type(c))
+
+# name = ''' Muhammad
+#            Nabeel
+#        '''
+# print(name)
+
+list = ['Nabeel', 26, 'Male', 'Karachi', 'Pakistan', 'Muslim']
+list[1] = 20
+print(list)
