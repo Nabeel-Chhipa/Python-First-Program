a7bea1c164d3c9e23e1793f18978c2407646ad25
Nabeel-Chhipa <nabeelchipa@gmail.com>
2024-07-06 10:20:43 +0500

    List methods

--- list.py
+++ list.py
@@ -2,3 +2,12 @@ employees = ['Nabeel', 'Usama', 'Saad', 'Uzair', 'Adnan', 'Amir', 'Furqan', 'Bil
 employees.append('Murtaza')
 employees.insert(4,'Asif')
 print('Murtaza' in employees)
+
+
+
+data = ['Nabeel', 'Usama', 'Saad', 'Uzair', 'Adnan', 'Amir', 'Furqan', 'Bilal']
+# data.append('test')
+# data.remove('test')
+# data.pop()
+# data.pop(6)
+print(data)
