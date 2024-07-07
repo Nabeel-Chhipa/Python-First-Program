018828b339b0b6cbe25afdda51673c54fd4cd09b
Nabeel-Chhipa <nabeelchipa@gmail.com>
2024-07-08 00:35:21 +0500

    for loop

---
+++ forLoop.py
@@ -0,0 +1,41 @@
+# data = [200, 1200, 3490, 771, 9820, 660, 1980]
+# total = 0
+# for item in data:
+#     total = total + item
+# print(total)
+
+# fruits = ['Apple', 'Banana', 'Cherry']
+# for item in fruits:
+#     print(item)
+
+# name = 'Muhammad Nabeel'
+# for _name in name:
+#     print(_name)
+
+# fruits = ['Apple', 'Banana', 'Banana', 'Cherry']
+# for item in fruits:
+#     if item == 'Banana':
+#         break
+#     print(item)
+
+# fruits = ['Apple', 'Banana', 'Cherry']
+# for item in fruits:
+#     if item == 'Banana':
+#         continue
+#     print(item)
+
+# for i in range(2, 30, 3):
+#     print(i)
+
+# for i in range(6):
+#     if i == 4:
+#         break
+#     print(i)
+# else:
+#     print('Loop end')
+
+# adj = ['red', 'big', 'testy']
+# fruits = ['Apple', 'Banana', 'Cherry']
+# for x in adj:
+#     for y in fruits:
+#         print(x, y)
