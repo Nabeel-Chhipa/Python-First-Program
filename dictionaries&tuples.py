6b00b940e0fe63720b88bfc57bc62f3d915e7376
Nabeel-Chhipa <nabeelchipa@gmail.com>
2024-07-09 18:46:24 +0500

    dictionaries & tuples

---
+++ dictionaries&tuples.py
@@ -0,0 +1,29 @@
+# data = {"nabeel":3172098121, "usama":3322324811}
+# data['saad'] = 13019237623
+# del data['saad']
+# print(data)
+
+# data = {
+#     'name1': 'Nabeel',
+#     'name2': 'Usama',
+#     'name3': 'Saad'
+# }
+# for key in data:
+#     print('key:', key, 'value', data[key])
+
+# for k,v in data.items():
+#     print('key: ', k, 'value: ', v)
+
+# checkName = 'name1' in data
+
+# data.clear()
+# print(data)
+
+
+
+# TUPLE
+# names = ('Nabeel', 'Usama', 'Saad', 'Zain', 'Zariyab', 'Faraz')
+# print(names)
+
+data = ('Nabeel', 27, 'Male', 'Pakistan', 'Karachi', 'Korangi', 'Male',)
+print(data[1])
