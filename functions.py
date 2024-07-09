bfeaf962362dd1a1911721947548df64c570af0a
Nabeel-Chhipa <nabeelchipa@gmail.com>
2024-07-09 11:23:01 +0500

    functions

---
+++ functions.py
@@ -0,0 +1,30 @@
+nabeel_exp = [2100, 3400, 3500]
+usama_exp = [200, 500, 700]
+# total=0
+# for nabeel_item in nabeel_exp:
+#     total = total + nabeel_item
+# print('Nabeel Total expense: ',total)
+
+# total = 0
+# for usama_item in usama_exp:
+#     total = total + usama_item
+# print('Usama Total expense: ',total)
+
+# def calculate_exp(exp):
+#     total = 0
+#     for item in exp:
+#         total = total + item
+#     return total
+# nabeel_total_exp = calculate_exp(nabeel_exp)
+# print('Nabeel total expense: ', nabeel_total_exp)
+
+# total = 0
+# def sum(num1, num2=2):
+#     total = num1 + num2
+#     return total
+# sum_total = sum(8)
+# print('total of sum: ', sum_total)
+
+def emp_name(*names):
+    print(names)
+emp_name('Nabeel', 'Usama', 'Saad', 'Zain', 'Zariyab', 'Faraz')
