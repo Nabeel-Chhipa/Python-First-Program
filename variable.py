712b893a60b252f84a7238fca5ddc1d2d51afb49
Muhammad Nabeel <nabeelchipa@gmail.com>
2024-10-23 12:56:49 -0700

    update variable file

--- variable.py
+++ variable.py
@@ -1,9 +1,16 @@
-name = 'Muhammad Nabeel'
-age1 = 26
-age2 = 26
-age3 = 27
+# name = 'Muhammad Nabeel'
+# age1 = 26
+# age2 = 26
+# age3 = 27
 
 # id() function is use to find a variable memory location
-print(id(age1), id(age2), id(age3))
+# print(id(age1), id(age2), id(age3))
 
-print('Name : ', name,'\nAge : ', age1)
+# print('Name : ', name,'\nAge : ', age1)
+
+# nabeel, usama, saad = 27, 28, 26
+# print(nabeel)
+# print(usama)
+
+# nabeel = usama = saad = 27
+# print(usama)
